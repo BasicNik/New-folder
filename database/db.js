357e14656e3e5c9f368a5e2bebde02fb289f0a8d
Nikhil <basicnik1798@gmail.com>
2024-01-25 00:48:11 +0530

    .

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://127.0.0.1:27017/task-manager';
+const mongoURI = 'mongodb://127.0.0.1:27017/task-management-mern';
 const options = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
